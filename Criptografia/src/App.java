509d506cece972bb0baa84833810e9d2ceba584e
Vinicius de Souza Salviano <122842052+ViniciusSalviano@users.noreply.github.com>
2026-04-30 19:30:55 -0300

    Adicionando um menu de opções ao programa

--- Criptografia/src/App.java
+++ Criptografia/src/App.java
@@ -5,18 +5,38 @@ import pacote.descriptografar;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] letras = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-        System.out.print("\nDigite uma palavra (minusculas): ");
-        String fraseOriginal = sc.nextLine();
-
-        // 1. Chama a criptografia
-        String textoEscondido = criptografar.funcaocriptografar(fraseOriginal, letras);
-        System.out.println("\nCriptografada: " + textoEscondido);
-
-        // 2. Chama a descriptografia
-        String textoRevelado = descriptografar.funcaoDescriptografar(textoEscondido, letras);
-        System.out.println("Descriptografada: " + textoRevelado+"\n");
+        char[] letras = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        String textoEscondido = "";
+        int op = -1; 
+        while (op != 3) {
+            System.out.println("\n ==== Menu ===");
+            System.out.println("1. Criptografar frase");
+            System.out.println("2. Descriptografar frase");
+            System.out.println("3. Sair do programa\n");
+            System.out.print("Digite o numero da opção desejada: ");
+            op = sc.nextInt();
+            sc.nextLine();
+            switch (op) {
+                case 1:
+                    System.out.print("\nDigite uma frase (somente letras minusculas): ");
+                    String fraseOriginal = sc.nextLine();
+                    textoEscondido = criptografar.funcaocriptografar(fraseOriginal, letras);
+                    System.out.println("\nCriptografada: " + textoEscondido);
+                    break;
+                case 2:
+                    String textoRevelado = descriptografar.funcaoDescriptografar(textoEscondido, letras);
+                    System.out.println("Descriptografada: " + textoRevelado+"\n");
+                    break;
+                case 3:
+                    System.out.println("Saindo do programa....");
+                        break;
+                    default:
+                        System.out.println("[ERRO] Numero invalido");
+                        break;
+                }
+            
+        }
 
         sc.close();
     }
